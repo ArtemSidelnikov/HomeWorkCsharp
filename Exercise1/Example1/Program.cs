9703da31f12100ec7e3f288d6bc7d06469da259e
ArtemSidelnikov <artem-sid@mail.ru>
2022-12-08 14:57:52 +0500

    First save

---
+++ Exercise1/Example1/Program.cs
@@ -0,0 +1,9 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.WriteLine("Введите число 1: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число 2: ");
+int b = int.Parse(Console.ReadLine()!);
+int max=a;
+if(b>a)max=b;
+Console.Write("max=");
+Console.WriteLine(max);
---
+++ Exercise1/Example4/Program.cs
@@ -0,0 +1,10 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+int b = 1;
+while (b<a+1)
+{
+    if(b%2==0) Console.Write (b);
+    if(b%2==0) Console.Write (", ");
+        b++;
+}
