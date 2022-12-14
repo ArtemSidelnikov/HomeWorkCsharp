6bad4d6a7cd9cef160c7437809bbb0ebedaac6de
ArtemSidelnikov <artem-sid@mail.ru>
2022-12-14 08:48:50 +0500

    Example2

---
+++ Exercise2/Example2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+if (num<100)
+{
+    Console.WriteLine("Нет третьей цифры");
+}
+else
+{
+       while (num >= 1000)
+    {
+         num = num/10;
+    }
+int a = num % 10;
+Console.WriteLine(a);
+}
