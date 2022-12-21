27595be9c9d9d830ff32af21be90206b9cca8631
ArtemSidelnikov <artem-sid@mail.ru>
2022-12-21 11:52:35 +0500

    Exercise3, Example3

---
+++ Exercise3/Example3/Program.cs
@@ -0,0 +1,9 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+int num2=1;
+while (num2<=num)
+{
+   Console.WriteLine(num2*num2*num2);
+   num2=num2+1; 
+}
