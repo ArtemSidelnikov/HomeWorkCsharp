23026f53b43ad709a552d82dfc5190b276f42ec0
ArtemSidelnikov <artem-sid@mail.ru>
2022-12-21 11:26:13 +0500

    Exercise3, Example2

---
+++ Exercise3/Example2/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве
+Console.WriteLine("Введите координаты первой точки x, y, z: ");
+float x1 = float.Parse (Console.ReadLine()!);
+float y1 = float.Parse (Console.ReadLine()!);
+float z1 = float.Parse (Console.ReadLine()!);
+Console.WriteLine("Введите координаты второй точки x, y, z: ");
+float x2 = float.Parse (Console.ReadLine()!);
+float y2 = float.Parse (Console.ReadLine()!);
+float z2 = float.Parse (Console.ReadLine()!);
+float Sum = (x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)+(z2-z1)*(z2-z1);
+Console.Write("Расстояние: ");
+Console.WriteLine(MathF.Sqrt(Sum)); 
